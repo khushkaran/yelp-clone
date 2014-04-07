afca9232dad0d3677b6fa9352b8fda7fb65914ab
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-04-07 11:39:15 +0100

    Added RSpec and route to /restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ YelpClone::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  get '/restaurants' => 'restaurants#index'
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
---
+++ spec/features/restaurants_feature_spec.rb
@@ -0,0 +1,12 @@
+require "spec_helper"
+
+describe 'the restaurants index page' do
+  context 'no restaurants have been added' do
+    it 'should display a warning message' do
+      visit '/restaurants'
+      expect(page).to have_content 'No restaurants yet'
+    end
+
+    
+  end
+end
