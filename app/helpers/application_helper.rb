b2d46f7e9dd2fa008798cdf392a9090933339570
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-04-09 14:07:19 +0100

    Extracted out flash_class hash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_class(type)
+    {
+      notice: "alert alert-info",
+      success: "alert alert-success",
+      error: "alert alert-danger",
+      alert: "alert alert-warning"
+    }[type]
+  end
 end
