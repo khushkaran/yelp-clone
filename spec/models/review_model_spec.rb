d8bb975e8f2c9e849e40cbebca34fa9c4bd95d61
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-04-09 11:34:16 +0100

    Added rating validations to review

--- spec/models/review_model_spec.rb
+++ spec/models/review_model_spec.rb
@@ -8,6 +8,17 @@ describe 'validations' do
     end
     it "rating" do
       review = Review.new(rating: nil)
+      expect(review).to have(2).errors_on(:rating)
+    end
+  end
+
+  context "rating is between 1 and 5" do
+    it "is not greater than 5" do
+      review = Review.new(rating: 7)
+      expect(review).to have(1).errors_on(:rating)
+    end
+    it "is not lower than 1" do
+      review = Review.new(rating: -7)
       expect(review).to have(1).errors_on(:rating)
     end
   end
